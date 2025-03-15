19bc07d72cb78e24128457f2285c34bda4f9056b
Nadeŭka <nadevko@riseup.net>
2025-03-15 22:49:49 +0300

    Dotnet will be better

--- shell.nix
+++ shell.nix
@@ -6,11 +6,24 @@
   },
   mkShell ? pkgs.mkShell,
   writeText ? pkgs.writeText,
-  clang-tools ? pkgs.clang-tools,
+  xorg ? pkgs.xorg,
+  dotnetCorePackages ? pkgs.dotnetCorePackages,
+  fontconfig ? pkgs.fontconfig,
 }:
+let
+  sdk = with dotnetCorePackages; combinePackages [ sdk_8_0 ];
+  deps = [
+    xorg.libX11
+    xorg.libICE
+    xorg.libSM
+    fontconfig
+  ];
+in
 mkShell rec {
   vscode-settings = writeText "settings.json" (
-    builtins.toJSON { "clangd.path" = "${clang-tools}/bin/clangd"; }
+    builtins.toJSON {
+      "dotnetAcquisitionExtension.sharedExistingDotnetPath" = DOTNET_ROOT;
+    }
   );
 
   packages = with pkgs; [
@@ -25,13 +38,14 @@ mkShell rec {
     tex-fmt
     inkscape-with-extensions
     python312Packages.pygments
-    clang-tools
-    cmake-format
-    cmake
-  ];
+
+    sdk
+  ] ++ deps;
+  LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath deps;
+  DOTNET_ROOT = "${sdk}/dotnet";
 
   shellHook = ''
     mkdir .vscode &>/dev/null
-    cp ${vscode-settings} .vscode/settings.json
+    cp --force ${vscode-settings} .vscode/settings.json
   '';
 }
