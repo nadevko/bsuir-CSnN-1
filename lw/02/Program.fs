ad88e9396b529023e1122448c313733bb61adfc3
Nadeŭka <nadevko@riseup.net>
2025-05-15 17:32:30 +0300

    Looks like working UDP version

--- lw/02/Program.fs
+++ lw/02/Program.fs
@@ -1,3 +1,55 @@
-﻿module internal Traceroute.Program
+﻿open System.Net
+open System.Net.Sockets
 
-printf ""
+let getResultString (ipAddress: IPAddress) =
+    try
+        let hostEntry = Dns.GetHostEntry(ipAddress)
+        if System.String.IsNullOrEmpty(hostEntry.HostName) then ipAddress.ToString()
+        else sprintf "%s (%s)" hostEntry.HostName (ipAddress.ToString())
+    with
+    | _ -> ipAddress.ToString()
+
+let traceroute (hostname : string) (maxHops : int) (port: int) (timeout: int) =
+    let destIP =
+        try
+            Dns.GetHostEntry(hostname).AddressList
+            |> Array.find (fun ip -> ip.AddressFamily = AddressFamily.InterNetwork)
+        with _ ->
+            failwithf "Host are not resolved: %s" hostname
+
+    use receiver =
+        new Socket (AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp)
+
+    receiver.ReceiveTimeout <- timeout
+    receiver.Bind (new IPEndPoint (IPAddress.Any, 0))
+    let destEP = new IPEndPoint (destIP, port)
+
+    printfn "traceroute to %s (%A), %d hops max" hostname destIP maxHops
+
+    let mutable isReached = false
+    let mutable ttl = 1
+
+    while ttl <= maxHops && not isReached do
+        use client = new UdpClient ()
+        client.Client.Ttl <- int16 ttl
+        client.Send ([| 0uy |], 1, destEP) |> ignore
+
+        let mutable result = "*"
+
+        try
+            let mutable buffer = Array.zeroCreate 512
+            let mutable remoteEP = new IPEndPoint (IPAddress.Any, 0) :> EndPoint
+            receiver.ReceiveFrom (buffer, &remoteEP) |> ignore
+            let remoteEP' = remoteEP :?> IPEndPoint
+            let remoteIP = remoteEP'.Address
+            result <- getResultString remoteIP
+            printfn "%2d  %s" ttl result
+
+            if remoteEP'.Address.Equals destIP then
+                isReached <- true
+        with :? SocketException ->
+            printfn "%2d  *" ttl
+
+        ttl <- ttl + 1
+
+traceroute "1.1.1.1" 30 33434 3000
